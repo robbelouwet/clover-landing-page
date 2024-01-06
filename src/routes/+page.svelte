c3922e3edad8e07f8af38cb80ae18ad2a0137e19
Robbe Louwet <robbe.louwet@nara.to>
2024-01-06 13:22:45 +0100

    init commit, sveltekit + tailwind + daisyUI template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="carousel carousel-center max-w p-4 space-x-4 bg-neutral rounded-box">
+	<div class="carousel-item">
+		<img
+			src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+			class="rounded-box"
+		/>
+	</div>
+	<div class="carousel-item">
+		<img
+			src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+			class="rounded-box"
+		/>
+	</div>
+	<div class="carousel-item">
+		<img
+			src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+			class="rounded-box"
+		/>
+	</div>
+	<div class="carousel-item">
+		<img
+			src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+			class="rounded-box"
+		/>
+	</div>
+	<div class="carousel-item">
+		<img
+			src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+			class="rounded-box"
+		/>
+	</div>
+	<div class="carousel-item">
+		<img
+			src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+			class="rounded-box"
+		/>
+	</div>
+	<div class="carousel-item">
+		<img
+			src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+			class="rounded-box"
+		/>
+	</div>
+</div>
