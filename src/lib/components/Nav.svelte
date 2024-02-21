faca31c1fe09945a49ef3cefc12930c596407991
Robbe Louwet <robbe.louwet@nara.to>
2024-02-21 17:51:52 +0100

    fixed login redirect host

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import { PUBLIC_BACKEND_HOST } from '$env/static/public';
+	import { onMount } from 'svelte';
 	import { CommandLine, Folder, Icon, Play, UserCircle } from 'svelte-hero-icons';
+
+	let host = '';
+
+	onMount(() => (host = window.location.host));
 </script>
 
 <div class="inset-x-0 top-0 navbar bg-base-100 p-0">
@@ -28,7 +33,7 @@
 				</a>
 			</li>
 			<li>
-				<a href={`${PUBLIC_BACKEND_HOST}/.auth/login/google?post_login_redirect_uri=/`}>
+				<a href={`${PUBLIC_BACKEND_HOST}/.auth/login/google?post_login_redirect_uri=${host}`}>
 					<Icon src={UserCircle} size="20" />
 					Login
 				</a>
